7d0c2a8aeb80abd70b4e609e3e12c59d2e0b8ac7
ToffaKrtek <toffa@toffakrtek.Dlink>
2020-12-16 18:14:37 +0700

    -Гугловская мапа

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,5 @@
 const password = 'youserviceadmin'
 module.exports = {
     mongoURI: `mongodb+srv://youservice:${password}@youservicedb.03hlu.mongodb.net/youservicedb?retryWrites=true&w=majority`,
-    jwt: 'dev-jwt',
-    MAP_API_KEY: 'AIzaSyAYXDqdOQH-GfnybkMkRUSNkM-4zodDI04'
+    jwt: 'dev-jwt'
 }
